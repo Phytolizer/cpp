a8f7a214fcd4f352504df88426134ff4e804d39a
Kyle Coffey <me@phytolizer.dev>
2022-03-03 20:40:23 -0600

    use spdlog

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,14 @@
 #include "cpp/charset.hpp"
 #include <cstdio>
-#include <fmt/format.h>
 #include <span>
+#include <spdlog/spdlog.h>
 #include <unicode/unistr.h>
 
 int main(int argc, char** argv) {
     std::span args{argv, static_cast<std::size_t>(argc)};
+    spdlog::set_pattern("[%l] %v");
     if (args.size() < 2) {
-        fmt::print(stderr, "not enough arguments provided\n");
+        spdlog::error("not enough arguments provided");
         return 1;
     }
     icu::UnicodeString source = cpp::open_file(args[1]);
