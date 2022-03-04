3ab86a08286ce1d964a298c891cf6a8852282bc4
Kyle Coffey <me@phytolizer.dev>
2022-03-03 20:31:43 -0600

    fix up some UB on invalid args

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,14 @@
 #include "cpp/charset.hpp"
+#include <cstdio>
+#include <fmt/format.h>
 #include <span>
 #include <unicode/unistr.h>
 
 int main(int argc, char** argv) {
     std::span args{argv, static_cast<std::size_t>(argc)};
+    if (args.size() < 2) {
+        fmt::print(stderr, "not enough arguments provided\n");
+        return 1;
+    }
     icu::UnicodeString source = cpp::open_file(args[1]);
 }
